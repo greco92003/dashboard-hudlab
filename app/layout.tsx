b7bb09882a264f0bfce12e5f03304486cba87daf
Greco Marques <greco92003@gmail.com>
2025-08-14 14:13:40 -0300

    teste3

--- app/layout.tsx
+++ app/layout.tsx
@@ -218,7 +218,7 @@ export default async function Layout({
               <SyncProvider>
                 <ThemeProvider
                   attribute="class"
-                  defaultTheme="light"
+                  defaultTheme="dark"
                   enableSystem
                   disableTransitionOnChange
                 >
